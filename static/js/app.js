97b09be8d62452845ce4fe8dc9fd17e9d1f83319
daKeshra7 <keshradamora@gmail.com>
2021-11-18 05:02:50 +0100

    working now

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,6 @@
 (function () {
   count = 0;
+  const modalBox = document.querySelector("#modal"); //Popup div conatiner
 
   //Select DOM elements
   const addCount = document.querySelector(".add-count");
@@ -9,10 +10,15 @@
   const emptyDiv = document.querySelector(".empty");
   const navCartContent = document.querySelector(".cart-content");
   const productCount = document.querySelector(".product-count");
-
+  const navCartPrice = document.querySelector(".chkt-price");
+  const amount = document.querySelector(".amount").innerHTML.slice(1, -3);
+  const checkOutCount = document.querySelector(".chkt-count");
+  const checkOutTotal = document.querySelector(".total-chkt");
+  ////
   productCount.innerHTML = parseInt(count);
-
-  //increment count
+  // amount = `$${amount}.00`;
+  //increment count and maneuve cart box that displays
+  //on .nav shopping cart icon hover
   addCount.addEventListener("click", function () {
     count++;
     productCount.innerHTML = count;
@@ -20,6 +26,12 @@
     countBadge.innerHTML = count;
     emptyDiv.style.display = "none";
     navCartContent.style.display = "block";
+    rAmount = `$${amount}.00`;
+    navCartPrice.innerHTML = rAmount;
+    checkOutCount.innerHTML = count;
+    checkOutTotal.innerHTML = `$${amount * count}.00`;
+
+    console.log(checkOutTotal.innerHTML);
   });
 
   //Decrement count
@@ -37,17 +49,33 @@
   });
 
   function showLightBox() {
-    const modalBox = document.querySelector("#modal"); //Popup div conatiner
+    // const modalBox = document.querySelector("#modal"); //Popup div conatiner
     const modalImg = document.getElementById("modalImg"); //the main image to switch content on popu
     const closeModal = document.querySelector(".pop-close"); //Close pop up modal box
     const popModalImg = document.getElementsByClassName("pop-modal");
     const popImgCont = document.getElementsByClassName("img-pop");
-    const imgSub = document.getElementsByClassName("img-subs");
     const lightImg = document.getElementById("lightImg");
     const lightModal = document.getElementsByClassName("light-modal");
     const backImgIcon = document.getElementById("prevIcon");
     const nextImgIcon = document.getElementById("nextIcon");
+    ////Navbar amd icons
+    const openNavIcon = document.querySelector(".icon-open"); //Navbar esponsive menu
+    const closeNavIcon = document.querySelector(".icon-close"); //Close navbar menu on obile screen
+    const navbar = document.querySelector(".navbar");
 
+    //nav bar oipne
+    openNavIcon.addEventListener("click", function () {
+      navbar.style.display = "inline-block";
+      openNavIcon.style.display = "none";
+      closeNavIcon.style.display = "block";
+    });
+
+    //Close navbar
+    closeNavIcon.addEventListener("click", function () {
+      navbar.style.display = "none";
+      closeNavIcon.style.display = "none";
+      openNavIcon.style.display = "block";
+    });
     //Next ligth box image
     c = 0;
     nextImgIcon.addEventListener("click", function nexOne() {
@@ -107,7 +135,6 @@
     //Add cart button
     const cartBtn = document.querySelector(".btn");
     // let priceSlash = document.querySelector(".text-strike").innerHTML;
-    const amount = document.querySelector(".amount").innerHTML;
 
     cartBtn.addEventListener("click", function () {
       result = parseFloat(amount) * 2;
@@ -122,5 +149,4 @@
   }
 
   showLightBox();
-  // showLightBox(lightModalClick);
 })();
